d6c2060196c13fbc8d554b3acf47ac9d76a97614
bcluc <21521898@gm.uit.edu.vn>
2024-06-16 02:33:25 +0700

    print pdf

--- app/src/main/java/com/example/shopper/customerview/notification/activity/DetailCustomerOrder.java
+++ app/src/main/java/com/example/shopper/customerview/notification/activity/DetailCustomerOrder.java
@@ -1,11 +1,18 @@
 package com.example.shopper.customerview.notification.activity;
 
+import android.graphics.Canvas;
+import android.graphics.pdf.PdfDocument;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -14,12 +21,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.shopper.R;
 import com.example.shopper.staffview.order.adapter.MyProductAdapter;
 import com.example.shopper.staffview.order.model.ItemOrder;
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,8 +46,80 @@ public class DetailCustomerOrder extends AppCompatActivity {
     private MyProductAdapter myProductAdapter;
     private ImageView img_avatar;
     private Button btn_exp_receipt;
+    private String avatar;
 
     public DetailCustomerOrder() {
+    }
+
+    private void convertXMLToPage(TextView tenNguoiMua, TextView tenND, TextView maND, TextView soDT, TextView diaChi, TextView tongTienThanhToan, TextView giaTriDonHang, TextView giamGia, TextView phiVanChuyen, TextView tongTien, String imageAvatar) {
+        //Inflate the Layout
+        TextView mTenNguoiMua, mTenND, mMaND, mSoDT, mDiachi, mtongTienThanhToan;
+        TextView mtxtGiaTriDonHang, mtxtGiamGia, mtxtPhiVanChuyen, mtxtTongTien;
+        ImageView mimg_avatar;
+        RecyclerView mrecyclerViewProducts;
+        View view = LayoutInflater.from(this).inflate(R.layout.item_invoice, null);
+        mTenNguoiMua = view.findViewById(R.id.tennguoimua_customer);
+        mMaND = view.findViewById(R.id.maND_customer);
+        mimg_avatar = view.findViewById(R.id.img_avatar);
+        mDiachi = view.findViewById(R.id.diachi_chitiet_customer);
+        mrecyclerViewProducts = view.findViewById(R.id.list_sanpham_customer);
+        mSoDT = view.findViewById(R.id.soDT_customer);
+        mTenND = view.findViewById(R.id.tenND_customer);
+
+        mtxtGiaTriDonHang = view.findViewById(R.id.giatridonhang_customer);
+        mtxtGiamGia = view.findViewById(R.id.discount_customer);
+        mtxtPhiVanChuyen = view.findViewById(R.id.phiVanChuyen_customer);
+        mtxtTongTien = view.findViewById(R.id.TongTien_customer);
+        mtongTienThanhToan = view.findViewById(R.id.total_customer);
+        mTenNguoiMua.setText(tenNguoiMua.getText());
+        mTenND.setText(tenND.getText());
+        mMaND.setText(maND.getText());
+        mSoDT.setText(soDT.getText());
+        mDiachi.setText(diaChi.getText());
+        mtxtGiaTriDonHang.setText(giaTriDonHang.getText());
+        mtxtGiamGia.setText(giamGia.getText());
+        mtxtTongTien.setText(tongTien.getText());
+        mtxtPhiVanChuyen.setText(phiVanChuyen.getText());
+        mtongTienThanhToan.setText(tongTienThanhToan.getText());
+        Picasso.get().load(imageAvatar).into(mimg_avatar);
+        mrecyclerViewProducts.setAdapter(myProductAdapter);
+        mrecyclerViewProducts.setLayoutManager(new GridLayoutManager(this, 1));
+
+        itemOrderList = new ArrayList<>();
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            this.getDisplay().getRealMetrics(displayMetrics);
+        } else this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.measure(View.MeasureSpec.makeMeasureSpec(displayMetrics.widthPixels, View.MeasureSpec.EXACTLY),
+                View.MeasureSpec.makeMeasureSpec(displayMetrics.heightPixels, View.MeasureSpec.EXACTLY));
+        PdfDocument document = new PdfDocument();
+        int viewWidth = view.getMeasuredWidth();
+        int viewHeight = view.getMinimumHeight();
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(1080, 1920, 1).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+        //Canvas
+        Canvas canvas = page.getCanvas();
+        view.draw(canvas);
+        //Finish the page
+        document.finishPage(page);
+        File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        String fileName = "Invoice" + Timestamp.now() + ".pdf";
+        File file = new File(downloadsDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            document.writeTo(fos);
+            document.close();
+            fos.close();
+            Toast.makeText(this, "Convert PDF Successfully!!!", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
@@ -61,7 +145,12 @@ public class DetailCustomerOrder extends AppCompatActivity {
         itemOrderList = new ArrayList<>();
         btn_exp_receipt = findViewById(R.id.btn_exp_receipt);
 
-
+        btn_exp_receipt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convertXMLToPage(TenNguoiMua, TenND, MaND, SoDT, Diachi, tongTienThanhToan, txtGiaTriDonHang, txtGiamGia, txtPhiVanChuyen, txtTongTien, avatar);
+            }
+        });
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,8 +169,7 @@ public class DetailCustomerOrder extends AppCompatActivity {
                     if (documentSnapshot.exists()) {
 
                         String state = documentSnapshot.getString("state");
-                        if(!state.equals("Delivered") )
-                        {
+                        if (!state.equals("Delivered")) {
                             btn_exp_receipt.setVisibility(View.GONE);
                             Log.d("State", "Visible");
                         }
@@ -111,8 +199,8 @@ public class DetailCustomerOrder extends AppCompatActivity {
                         DocumentReference nguoidungref = nguoidung.document(maND);
                         nguoidungref.get().addOnCompleteListener(nguoidungdocument -> {
                             if (nguoidungdocument.getResult().exists()) {
-                                String anhDaiDien = nguoidungdocument.getResult().getString("avatar");
-                                Picasso.get().load(anhDaiDien).into(img_avatar);
+                                avatar = nguoidungdocument.getResult().getString("avatar");
+                                Picasso.get().load(avatar).into(img_avatar);
                             }
                         });
 
